cac96ed27ab1d123ef6b437d88cc99bfc3dba757
seheerst <seherst9@gmail.com>
2023-07-19 14:56:13 +0300

    feat: UIHelper methods added

--- lib/ui/widgets/appTitle.dart
+++ lib/ui/widgets/appTitle.dart
@@ -1,29 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:pokemon_app/constants/constants.dart';
+import 'package:pokemon_app/constants/ui_helper.dart';
 
 class AppTitle extends StatelessWidget {
   AppTitle({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Align(
-            alignment: Alignment.topLeft,
-            child: Text(
-              Constants.title,
-              style: Constants.getTitleStyle(),
-            )),
-        Align(
-          alignment: Alignment.topRight,
-          child: Image.asset(
-            Constants.pokemonUrl,
-            width: 150,
-            height: 150,
-            fit: BoxFit.fitWidth,
-          ),
-        )
-      ],
+    return SafeArea(
+      child: SizedBox(
+        height: UIHelper.getAppTitleHeight(),
+        child: Stack(
+          children: [
+            Padding(
+              padding: UIHelper.getDefaultPadding(),
+              child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    Constants.title,
+                    style: Constants.getTitleStyle(),
+                  )),
+            ),
+            Align(
+              alignment: Alignment.topRight,
+              child: Image.asset(
+                Constants.pokemonUrl,
+                width: ScreenUtil().orientation == Orientation.portrait
+                    ? 0.2.sh
+                    : 0.2.sw,
+                fit: BoxFit.fitWidth,
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
