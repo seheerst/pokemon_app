530bef1dcdf5edbab3b6744d5c8c274e8f72e4c3
seheerst <seherst9@gmail.com>
2023-07-19 14:55:20 +0300

    feat: constants added

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -10,9 +10,24 @@ class Constants {
     return const TextStyle(
       color: Colors.white,
       fontWeight: FontWeight.bold,
-      fontSize: 45,
+      fontSize: 48,
     );
   }
 
   static const String pokemonUrl = "images/pokeball.png";
+
+  static TextStyle getPokeNameStyle() {
+    return const TextStyle(
+      color: Colors.white,
+      fontWeight: FontWeight.bold,
+      fontSize: 30,
+    );
+  }
+
+  static TextStyle getPokeTypeStyle() {
+    return const TextStyle(
+      color: Colors.white,
+      fontSize: 20,
+    );
+  }
 }
