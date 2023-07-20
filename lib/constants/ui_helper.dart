fbe4916ad229343b624bfcd013dcfb68043281e6
seheerst <seherst9@gmail.com>
2023-07-20 13:00:14 +0300

    feat: getIconPadding method added

--- lib/constants/ui_helper.dart
+++ lib/constants/ui_helper.dart
@@ -49,4 +49,13 @@ class UIHelper{
     }
   }
 
+  static EdgeInsets getIconPadding(){
+    if(ScreenUtil().orientation == Orientation.portrait){
+      return EdgeInsets.all(10.h);
+
+    }else{
+      return EdgeInsets.all(4.w);
+    }
+  }
+
 }
